97189c04e38214c71d4684b7b30ad7695354e531
burakansu <burakkansu9@gmail.com>
2022-12-13 12:55:32 +0300

    Create Database Auto EF Core

---
+++ Controllers/ControllerBase.cs
@@ -0,0 +1,16 @@
+﻿using Data.Context;
+
+namespace LMS_Api.Controllers
+{
+    public class ControllerBase
+    {
+        public ControllerBase()
+        {
+            using (Context x = new Context())
+            {
+                x.Database.EnsureCreated();
+            }
+        }
+
+    }
+}
